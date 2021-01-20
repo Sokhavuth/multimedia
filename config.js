3636244b2af7f2b702d18b4c4a1d33ffc5256751
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-01-20 19:39:17 +0700

    Make it better

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+var vdict = {};
+vdict['site_title'] = 'ពហុព័ត៌មាន';
+
+module.exports = vdict;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
+// routes/index.js
 var express = require('express');
 var router = express.Router();
+var vdict = require('../config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', vdict);
 });
 
 module.exports = router;
