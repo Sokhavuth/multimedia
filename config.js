41aa199f27f15145c65eed8e67d42071fcccf762
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-01-31 11:55:03 +0700

    Make it better

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-var vdict = {};
-vdict.site_title = 'ពហុព័ត៌មាន';
-vdict.databaseAccess = 'mongodb+srv://khmerweb:Ghtz4rOJIXLS5XEA@khmerjs.jvhqu.mongodb.net/multimediadb?retryWrites=true&w=majority';
-vdict.message = '';
-vdict.dashboardLimit = 5;
-vdict.edited = false;
-
-module.exports = vdict;
